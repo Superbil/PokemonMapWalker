3ae0964319427da7b325cadee5c7e1181becc425
Superbil <superbil@gmail.com>
2019-06-29 03:38:23 +0800

    Call selectLocation with parameters at ApplyGPX script
    - use swift to call apple script
    
    Ref:
    https://gist.github.com/chbeer/3666e4b7b2e71eb47b15eaae63d4192f

--- SkyWalker/SkyWalker/Falcon.swift
+++ SkyWalker/SkyWalker/Falcon.swift
@@ -9,31 +9,57 @@ import Carbon
 
 class Falcon {
 
-  private var applyGpxScript: NSAppleScript!
+  public var projectName: String = "LocationSimulation"
+  public var menuItemName: String = "R2-D2"
+
+  private var applyGpxScript: NSAppleScript?
 
   init() {
     let name = "ApplyGPX"
     guard let path = Bundle.main.path(forResource: name, ofType: "scpt") else {
-      assertionFailure("\(name).scpt Script not found.")
+      assertionFailure("\(name).scpt not found.")
       return
     }
     let url = NSURL(fileURLWithPath: path)
     var errors: NSDictionary?
-    self.applyGpxScript = NSAppleScript(contentsOf: url as URL, error: &errors)
+    guard let script = NSAppleScript(contentsOf: url as URL, error: &errors) else {
+      assertionFailure("\(name).scpt script init failed.")
+      return
+    }
+    self.applyGpxScript = script
+  }
+
+  private func selectLocation(projectName: String, simulateLocation: String) {
+    guard let script = applyGpxScript else {
+      debugPrint("applyGpxScript is null")
+      return
+    }
+
+    let handler = NSAppleEventDescriptor(string: "selectLocation")
+
+    let parameters = NSAppleEventDescriptor(listDescriptor: ())
+    parameters.insert(NSAppleEventDescriptor(string: projectName), at: 1)
+    parameters.insert(NSAppleEventDescriptor(string: simulateLocation), at: 2)
+
+    var psn = ProcessSerialNumber(highLongOfPSN: UInt32(0), lowLongOfPSN: UInt32(kCurrentProcess))
+    let target = NSAppleEventDescriptor(descriptorType: typeProcessSerialNumber, bytes: &psn, length: MemoryLayout<ProcessSerialNumber>.size)
+    let event = NSAppleEventDescriptor.appleEvent(withEventClass: AEEventClass(kASAppleScriptSuite),
+                                                  eventID: AEEventID(kASSubroutineEvent),
+                                                  targetDescriptor: target,
+                                                  returnID: AEReturnID(kAutoGenerateReturnID),
+                                                  transactionID: AETransactionID(kAnyTransactionID))
+
+    event.setParam(handler, forKeyword: AEKeyword(keyASSubroutineName))
+    event.setParam(parameters, forKeyword: AEKeyword(keyDirectObject))
+
+    var errors: NSDictionary?
+    script.executeAppleEvent(event, error: &errors)
     if let errors = errors {
-      debugPrint("Error creating AppleScript: \(errors.description)")
+      debugPrint("Error executing AppleScript: \(errors.description)")
     }
   }
 
   func jumpToLightSpeed() {
-    if applyGpxScript.isCompiled == false {
-      debugPrint("applyGpxScript is not compiled")
-      return
-    }
-    var errorDict: NSDictionary?
-    applyGpxScript.executeAndReturnError(&errorDict)
-    if errorDict != nil {
-      debugPrint("Error executing AppleScript: \(errorDict?.description ?? "")")
-    }
+    selectLocation(projectName: projectName, simulateLocation: menuItemName)
   }
 }
