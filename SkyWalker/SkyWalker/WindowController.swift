4ccb9e44d3a0308e5c471df26b1cf1f1ffae685a
Superbil <superbil@gmail.com>
2019-07-13 01:49:06 +0800

    Disable keyboard event
    - flow had infinite loop

--- SkyWalker/SkyWalker/WindowController.swift
+++ SkyWalker/SkyWalker/WindowController.swift
@@ -43,18 +43,18 @@ class WindowController : NSWindowController {
     }
 
     override func keyDown(with event: NSEvent) {
-        if let viewController = contentViewController as? ViewController {
-            viewController.handleKeyDown(event: event)
-            return
-        }
+//        if let viewController = contentViewController as? ViewController {
+//            viewController.handleKeyDown(event: event)
+//            return
+//        }
         super.keyDown(with: event)
     }
 
     override func keyUp(with event: NSEvent) {
-        if let viewController = contentViewController as? ViewController {
-            viewController.handleKeyUp(event: event)
-            return
-        }
+//        if let viewController = contentViewController as? ViewController {
+//            viewController.handleKeyUp(event: event)
+//            return
+//        }
         super.keyUp(with: event)
     }
 }
