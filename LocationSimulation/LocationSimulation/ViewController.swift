dc3764764e7ddafc45acb201c28baa828457eb9c
Superbil <superbil@gmail.com>
2019-07-17 01:59:01 +0800

    Add button to open map app on LocationSimulation

--- LocationSimulation/LocationSimulation/ViewController.swift
+++ LocationSimulation/LocationSimulation/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    @IBOutlet private weak var actionButton: UIButton! {
+        didSet {
+            actionButton.addTarget(self, action: #selector(selectAction(_:)), for: .touchUpInside)
+        }
+    }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        debugPrint("Receive Memory Warning")
+    }
 
+    @objc func selectAction(_ sender: UIButton) {
+        let c = UIAlertController(title: "Select map app",
+                                  message: nil,
+                                  preferredStyle: .actionSheet)
+        let appleMapURL = URL(string: "http://maps.apple.com/maps?")!
+        if UIApplication.shared.canOpenURL(appleMapURL) {
+            c.addAction(UIAlertAction(title: "Apple Map", style: .default, handler: { _ in
+                UIApplication.shared.openURL(appleMapURL)
+            }))
+        }
+        let googleMapURL = URL(string: "comgooglemaps://?")!
+        if UIApplication.shared.canOpenURL(googleMapURL) {
+            c.addAction(UIAlertAction(title: "Google Map", style: .default, handler: { action in
+                UIApplication.shared.openURL(googleMapURL)
+            }))
+        }
+        c.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(c, animated: true)
+    }
 }
 
